67726fc4a6d8781a1ac424cc594653a937b69c5d
Jami H <K4na@iki.fi>
2025-10-30 09:20:17 +0200

    created base vue turborepo

---
+++ apps/web/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <span class="text-2xl text-red-500">Hello World</span>
+    <TheWelcome />
+  </main>
+</template>
