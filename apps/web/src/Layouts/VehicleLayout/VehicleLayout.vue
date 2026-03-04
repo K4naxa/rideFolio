b6e8455dfc965f0c27e300de0ae9ba9890c53e6c
Jami H <K4na@iki.fi>
2026-03-04 17:02:54 +0200

    style: adjust z-index for vehicle navigation tabs in VehicleLayout

--- apps/web/src/Layouts/VehicleLayout/VehicleLayout.vue
+++ apps/web/src/Layouts/VehicleLayout/VehicleLayout.vue
@@ -53,7 +53,7 @@ const VEHICLE_TABS = computed<VehicleTab[]>(() => [
 
     <!-- Vehicle navigation tabs -->
     <div class="flex flex-1 flex-col">
-      <nav class="bg-background sticky top-(--app-header-height) z-20 mb-0 h-(--vehicle-navbar-height) text-sm">
+      <nav class="bg-background sticky top-(--app-header-height) z-10 mb-0 h-(--vehicle-navbar-height) text-sm">
         <!-- Mobile nav -->
         <MainContentWrapper class="px-1">
           <Tabs class="w-full">
