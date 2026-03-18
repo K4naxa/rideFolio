f82136c4f78f840cc988209742b02c8e5f7421b8
Jami H <K4na@iki.fi>
2026-03-18 10:53:16 +0200

    fix: fixed aria-hiddden bug from vaul drawer with blurOnClick

--- apps/web/src/components/ui/drawer/DrawerTrigger.vue
+++ apps/web/src/components/ui/drawer/DrawerTrigger.vue
@@ -3,10 +3,14 @@ import type { DrawerTriggerProps } from "vaul-vue";
 import { DrawerTrigger } from "vaul-vue";
 
 const props = defineProps<DrawerTriggerProps>();
+
+function blurOnClick(event: MouseEvent) {
+  (event.currentTarget as HTMLElement)?.blur();
+}
 </script>
 
 <template>
-  <DrawerTrigger data-slot="drawer-trigger" v-bind="props">
+  <DrawerTrigger data-slot="drawer-trigger" v-bind="props" @click="blurOnClick">
     <slot />
   </DrawerTrigger>
 </template>
