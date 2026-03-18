4400abc2b46ac1d537b52a70ff59f0b649f397b3
Jami H <K4na@iki.fi>
2026-03-18 13:21:19 +0200

    feat: created reusable SectionHeader

---
+++ apps/web/src/components/ui/SectionHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+import { type HTMLAttributes } from "vue";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <h4 :class="twMerge('text-muted-foreground text-xs font-medium tracking-wide uppercase', props.class)">
+    <slot />
+  </h4>
+</template>
+
+<style scoped></style>
